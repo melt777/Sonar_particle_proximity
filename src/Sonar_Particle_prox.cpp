fd63d1f4f3362795f45bc21249891bd37fefd219
melt777 <meltbrink@c-r-s.co.za>
2019-11-20 13:39:08 +0200

    1st commit

---
+++ src/Sonar_Particle_prox.cpp
@@ -0,0 +1,27 @@
+/******************************************************/
+//       THIS IS A GENERATED FILE - DO NOT EDIT       //
+/******************************************************/
+
+#include "application.h"
+#line 1 "c:/aacrs/02_12/markvisser/git_clone/sonar_particle_proximity/Sonar_Particle_prox/src/Sonar_Particle_prox.ino"
+/*
+ * Project Sonar_Particle_prox
+ * Description:
+ * Author:
+ * Date:
+ */
+
+// setup() runs once, when the device is first turned on.
+void setup();
+void loop();
+#line 9 "c:/aacrs/02_12/markvisser/git_clone/sonar_particle_proximity/Sonar_Particle_prox/src/Sonar_Particle_prox.ino"
+void setup() {
+  // Put initialization like pinMode and begin functions here.
+
+}
+
+// loop() runs over and over again, as quickly as it can execute.
+void loop() {
+  // The core of your code will likely live here.
+
+}
